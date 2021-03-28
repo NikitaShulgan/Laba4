589497b386eab29cae12395193bfe1bce1c94b73
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-28 23:27:48 +0300

    Update train_a.py

--- train_a.py
+++ train_a.py
@@ -42,8 +42,8 @@ TRAIN_SIZE = 12786
 # )
 
 def augment(image, label):
-  bright = tf.image.adjust_brightness(image, delta=0.1)
-  contrast = tf.image.adjust_contrast(bright, contrast_factor=2)
+  bright = tf.image.adjust_brightness(image, delta=0.2)
+  contrast = tf.image.adjust_contrast(bright, contrast_factor=3)
   return contrast, label
 
 
