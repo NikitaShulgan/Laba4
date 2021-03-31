36c7a012fe4e8cfbf960cb101d9b4ff126090e00
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-31 03:13:33 +0300

    Update train_d.py

--- train_d.py
+++ train_d.py
@@ -31,45 +31,6 @@ NUM_CLASSES = 20
 RESIZE_TO = 224
 TRAIN_SIZE = 12786
 
-# img_augmentation = keras.Sequential(
-#     [
-#         preprocessing.RandomRotation(factor=0.15),
-#         preprocessing.RandomTranslation(height_factor=0.1, width_factor=0.1),
-#         preprocessing.RandomFlip(),
-#         preprocessing.RandomContrast(factor=0.1),
-#     ]
-# )
-
-# def augment(image, label):
-#   bright = tf.image.adjust_brightness(image, delta=0.1)
-#   contrast = tf.image.adjust_contrast(image, 2)
-#   return image, label
-
-
-# resize_and_rescale = tf.keras.Sequential([
-#   layers.experimental.preprocessing.Resizing(RESIZE_TO, RESIZE_TO),
-#   layers.experimental.preprocessing.Rescaling(1./255)
-# ])
-
-# def augment(image_label, seed):
-#   image, label = image_label
-#   #image, label = resize_and_rescale(image, label)
-# #   tf.image.stateless_random_brightness(image, max_delta, seed)
-# #   tf.image.stateless_random_contrast(image, lower, upper, seed)
-#   image = tf.image.resize_with_crop_or_pad(image, IMG_SIZE + 6, IMG_SIZE + 6)
-#   # Make a new seed
-#   new_seed = tf.random.experimental.stateless_split(seed, num=1)[0, :]
-#   # Random crop back to the original size
-# #   image = tf.image.stateless_random_crop(
-# #       image, size=[IMG_SIZE, IMG_SIZE, 3], seed=seed)
-#   # Random brightness
-#   image = tf.image.stateless_random_brightness(
-#       image, max_delta=0.5, seed=new_seed)
-# #   image = tf.clip_by_value(image, 0, 1)
-#   return image, label
-
-# counter = tf.data.experimental.Counter()
-# train_ds = tf.data.Dataset.zip((train_datasets, (counter, counter)))
 
 def parse_proto_example(proto):
   keys_to_features = {
@@ -92,7 +53,7 @@ def create_dataset(filenames, batch_size):
   :tfrecords_files: Mask to collect tfrecords file of dataset
   :returns: tf.data.Dataset
   """
-  #.map(augment, parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)
+
   return tf.data.TFRecordDataset(filenames)\
     .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)\
     .cache()\
@@ -102,30 +63,13 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = tf.keras.layers.GaussianNoise(stddev=0.3)(inputs)
-  #x = img_augmentation(inputs)
-  #x = tf.keras.preprocessing.image.random_brightness(x, 3.0)
+  x = tf.keras.layers.GaussianNoise(stddev=0.1)(inputs)
   x = EfficientNetB0(include_top=False, input_tensor=x, weights="imagenet")
   x.trainable = False
   x = layers.GlobalAveragePooling2D()(x.output)
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation="softmax")(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
-# def exp_decay(epoch):
-#    initial_lrate = 0.01
-#    k = 0.6
-#    lrate = initial_lrate * exp(-k*t)
-#    return lrate
-
-def step_decay(epoch):
-   initial_lrate = 0.1
-   drop = 0.6
-   epochs_drop = 5.0
-   lrate = initial_lrate * math.pow(drop,  
-           math.floor((1+epoch)/epochs_drop))
-   return lrate
-  
-lrate = LearningRateScheduler(step_decay)
 
 
 def main():
@@ -141,7 +85,7 @@ def main():
   model = build_model()
 
   model.compile(
-    optimizer=tf.optimizers.Adam(),
+    optimizer=tf.optimizers.Adam(0.001),
     loss=tf.keras.losses.categorical_crossentropy,
     metrics=[tf.keras.metrics.categorical_accuracy],
   )
