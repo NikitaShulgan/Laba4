3a6bac755370d163fe1ee5ee73d3d62023ed1cbf
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-31 09:27:58 +0300

    Update train_4.py

--- train_4.py
+++ train_4.py
@@ -38,8 +38,8 @@ img_augmentation = keras.Sequential(
 )
 
 def augment(image, label):
-  bright = tf.image.adjust_brightness(image, delta=0.3)
-  contrast = tf.image.adjust_contrast(bright, contrast_factor=3)
+  bright = tf.image.adjust_brightness(image, delta=0.2)
+  contrast = tf.image.adjust_contrast(bright, contrast_factor=2)
   crop = tf.image.random_crop(contrast, [RESIZE_TO, RESIZE_TO, 3])
   return crop, label
 
@@ -75,7 +75,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = tf.keras.layers.GaussianNoise(stddev=0.1)(inputs)
+  x = tf.keras.layers.GaussianNoise(stddev=0.05)(inputs)
   x = img_augmentation(x)
   #x = img_augmentation(inputs)
   #x = tf.keras.preprocessing.image.random_brightness(x, 3.0)
